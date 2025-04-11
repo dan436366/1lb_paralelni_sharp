61bdaf1b3a0c4c286b55ac6315c295ee1fd9f1a6
dan436366 <mavpadan@gmail.com>
2025-04-11 15:27:27 +0300

    second commit

--- SequenceSumThread/Program.cs
+++ SequenceSumThread/Program.cs
@@ -1,65 +1,150 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
-namespace SequenceSumThread
+class Program
 {
-    class SequenceSumThread
+    static void Main()
     {
-        private int id;     
-        private int step;         
-        private bool running = true;
-        private long sum = 0;  
-        private int count = 0;    
+        Console.Write("Enter number of threads: ");
+        int threadCount = int.Parse(Console.ReadLine());
 
-        public SequenceSumThread(int id, int step)
+        Random random = new Random();
+        int[] steps = new int[threadCount];
+        int[] delays = new int[threadCount];
+
+        for (int i = 0; i < threadCount; i++)
         {
-            this.id = id;
-            this.step = step;
+            steps[i] = random.Next(1, 6);
+            delays[i] = random.Next(1, 6) * 1000;
         }
 
-        public void Stop() => running = false;
+        SumThread[] workers = new SumThread[threadCount];
 
-        public void Run()
+        for (int i = 0; i < threadCount; i++)
         {
-            int current = 0;
-            while (running)
-            {
-                sum += current;  
-                current += step; 
-                count++;
-                Thread.Sleep(50); 
-            }
-
-            Console.WriteLine($"Thread {id}: sum = {sum}, addends = {count}");
+            workers[i] = new SumThread(i + 1, steps[i]);
+            workers[i].Start();
         }
 
-        public static void Main(string[] args)
+        ControllerThread controller = new ControllerThread(workers, delays);
+        controller.Start();
+        controller.Join();
+
+    }
+}
+
+class SumThread
+{
+    private readonly int id;
+    private readonly int step;
+    private volatile bool running = true;
+
+    private long sum = 0;
+    private long count = 0;
+
+    private Thread thread;
+
+    public SumThread(int id, int step)
+    {
+        this.id = id;
+        this.step = step;
+    }
+
+    public void Start()
+    {
+        thread = new Thread(Run);
+        thread.Start();
+    }
+
+    private void Run()
+    {
+        int current = 0;
+
+        long startTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+
+        while (running)
         {
-            Console.Write("Enter number of threads: ");
-            int numThreads = int.Parse(Console.ReadLine());
+            sum += current;
+            current += step;
+            count++;
 
-            Console.Write("Enter step of threads: ");
-            int step = int.Parse(Console.ReadLine());
-
-            SequenceSumThread[] workers = new SequenceSumThread[numThreads];
-            Thread[] threads = new Thread[numThreads];
-
-            for (int i = 0; i < numThreads; i++)
+            long currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            if (currentTime - startTime >= 100) 
             {
-                workers[i] = new SequenceSumThread(i + 1, step);
-                threads[i] = new Thread(new ThreadStart(workers[i].Run));
-                threads[i].Start();
+                startTime = currentTime; 
+            }
+        }
+
+        Console.WriteLine($"Thread #{id} sum = {sum}, addends = {count}");
+    }
+
+    public void StopRunning()
+    {
+        running = false;
+    }
+
+    public void Join()
+    {
+        thread.Join();
+    }
+}
+
+class ControllerThread
+{
+    private readonly SumThread[] threads;
+    private readonly int[] delays;
+    private Thread controller;
+
+    public ControllerThread(SumThread[] threads, int[] delays)
+    {
+        this.threads = threads;
+        this.delays = delays;
+    }
+
+    public void Start()
+    {
+        controller = new Thread(Run);
+        controller.Start();
+    }
+
+    public void Join()
+    {
+        controller.Join();
+    }
+
+    private void Run()
+    {
+        long startTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+        bool[] stopped = new bool[threads.Length];
+
+        while (true)
+        {
+            long currentTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            bool allStopped = true;
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                if (!stopped[i])
+                {
+                    allStopped = false;
+
+                    if (currentTime - startTime >= delays[i])
+                    {
+                        threads[i].StopRunning();
+                        stopped[i] = true;
+                    }
+                }
             }
 
-            for (int i = 0; i < numThreads; i++)
+            if (allStopped)
             {
-                Thread.Sleep((i + 1) * 1000);
-                workers[i].Stop();
+                break;
             }
+        }
+
+        foreach (var t in threads)
+        {
+            t.Join();
         }
     }
 }
